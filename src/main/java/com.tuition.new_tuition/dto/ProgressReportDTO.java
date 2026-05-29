9166eafaccf533f592cbc06045b265e5a517445e
DinethmiPeiris <thinupeiris04@gmail.com>
2026-05-29 14:18:13 +0530

    Refactor ProgressReportDTO to remove student fields

--- src/main/java/com.tuition.new_tuition/dto/ProgressReportDTO.java
+++ src/main/java/com.tuition.new_tuition/dto/ProgressReportDTO.java
@@ -2,26 +2,26 @@ package com.tuition.new_tuition.dto;
 
 public class ProgressReportDTO {
 
-    private String studentName;
-    private String studentEmail;
     private String subject;
+    private String batchName;
+    private int studentCount;
     private int totalExams;
     private double averagePercentage;
     private long passCount;
     private long failCount;
     private long pendingCount;
 
-    public ProgressReportDTO(String studentName,
-                             String studentEmail,
-                             String subject,
+    public ProgressReportDTO(String subject,
+                             String batchName,
+                             int studentCount,
                              int totalExams,
                              double averagePercentage,
                              long passCount,
                              long failCount,
                              long pendingCount) {
-        this.studentName = studentName;
-        this.studentEmail = studentEmail;
         this.subject = subject;
+        this.batchName = batchName;
+        this.studentCount = studentCount;
         this.totalExams = totalExams;
         this.averagePercentage = averagePercentage;
         this.passCount = passCount;
@@ -29,35 +29,12 @@ public class ProgressReportDTO {
         this.pendingCount = pendingCount;
     }
 
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public String getStudentEmail() {
-        return studentEmail;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public int getTotalExams() {
-        return totalExams;
-    }
-
-    public double getAveragePercentage() {
-        return averagePercentage;
-    }
-
-    public long getPassCount() {
-        return passCount;
-    }
-
-    public long getFailCount() {
-        return failCount;
-    }
-
-    public long getPendingCount() {
-        return pendingCount;
-    }
+    public String getSubject()           { return subject; }
+    public String getBatchName()         { return batchName; }
+    public int getStudentCount()         { return studentCount; }
+    public int getTotalExams()           { return totalExams; }
+    public double getAveragePercentage() { return averagePercentage; }
+    public long getPassCount()           { return passCount; }
+    public long getFailCount()           { return failCount; }
+    public long getPendingCount()        { return pendingCount; }
 }
