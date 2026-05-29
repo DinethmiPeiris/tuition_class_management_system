236c9f7e836d70e59cc5b9ac7c470a544818e64f
DinethmiPeiris <thinupeiris04@gmail.com>
2026-05-29 14:23:42 +0530

    Add deleteByExamId method to QuestionRepository

--- src/main/java/com.tuition.new_tuition/repository/QuestionRepository.java
+++ src/main/java/com.tuition.new_tuition/repository/QuestionRepository.java
@@ -2,9 +2,14 @@ package com.tuition.new_tuition.repository;
 
 import com.tuition.new_tuition.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
     List<Question> findByExamId(Long examId);
+
+    @Transactional
+    void deleteByExamId(Long examId);
 }
